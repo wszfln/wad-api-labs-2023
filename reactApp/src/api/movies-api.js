5c21b1fffbd09042288e0c798bca1a4668ccab16
Yingying Lu <20095278@mail.wit.ie>
2023-12-08 16:11:18 +0000

    Topic 12 - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=62e9b1532f93f28c70bdb47f5ad15a3f&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
